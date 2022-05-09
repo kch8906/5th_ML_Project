9eb44f946bcd7ddf58c22be29bbe57a71ec31013
kch8906 <kch8906@naver.com>
2022-05-10 00:23:57 +0900

    Update Django

--- Eyes detection-Django/team5_project_django/co_eyes/views.py
+++ Eyes detection-Django/team5_project_django/co_eyes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from .models import Predictlabel, Temp
+from django.shortcuts import redirect, render
 
 
 # Create your views here.
 def co_eyes(request):
-    pass
-    return render(request, "main.html")
+    class_object = Temp.objects.last()
+    return render(request, "main.html", {'class_object': class_object})
