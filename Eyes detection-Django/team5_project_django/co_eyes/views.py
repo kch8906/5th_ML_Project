f8fba5ada7ec9fc44831f9cdca2611f054df46b8
kch8906 <kch8906@naver.com>
2022-05-16 14:21:03 +0900

    Upload final project

--- Eyes detection-Django/team5_project_django/co_eyes/views.py
+++ Eyes detection-Django/team5_project_django/co_eyes/views.py
@@ -1,12 +1,15 @@
 from .models import Predictlabel, Temp
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 import joblib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from time import localtime, strftime
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+
+
+
 
 def predict(request):
     object_name = Temp.objects.last()
@@ -18,11 +21,12 @@ def predict(request):
     humid = float(humid)
     co2 = float(co2)
 
-    df = pd.read_csv('/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/csv/temp.csv')
+    df = pd.read_csv(
+        '/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/csv/temp.csv')
     x = df.drop('label', axis=1, inplace=False)
 
-
-    estimator = joblib.load(r'/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/ml_model/Temp_LinearRegression.pkl')
+    estimator = joblib.load(
+        r'/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/ml_model/Temp_LinearRegression.pkl')
 
     x_test = [[temp, humid, co2]]
     scaler = StandardScaler()
@@ -30,21 +34,25 @@ def predict(request):
     test_pred2 = estimator.predict(test_scaled)
     y_pred_proba = np.floor(estimator.predict_proba(test_scaled)[:, 0] * 100)
 
-
-
     df = pd.DataFrame(list(Predictlabel.objects.all().values()))
-    df['register_date'] = df['register_date'].dt.day
-    df = df.groupby('register_date').max()
 
+    df['day'] = df['register_date'].dt.day
+    df = df.groupby('day').max()
+    df = df.reset_index()
+    df['year'] = df['register_date'].dt.strftime('%Y-%m-%d')
+
+
+    plt.figure(figsize=(10, 5), facecolor='white', edgecolor='white')
     plt.grid(True)
-    sns.lineplot(df.index,df['total_sleep_count'],color='slateblue', marker='o')
-    plt.fill_between(df.index,df['total_sleep_count'],color='lightpink', alpha=0.4)
-    plt.xticks(df.index)
-    plt.yticks(df['total_sleep_count'])
-    plt.xlabel('Date')
-    plt.ylabel('Sleep_count')
-    plt.title('Number of drowsy driving',fontsize=16)
 
-    plt.savefig('/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/img')
+    sns.lineplot(df['year'], df['total_sleep_count'], color='white', marker='o')
+    plt.fill_between(df['year'], df['total_sleep_count'], color='white', alpha=0.5)
+    plt.xticks(df['year'], color='white')
+    plt.yticks(df['total_sleep_count'], color='white')
+    plt.xlabel('Date', color='white', loc='right', labelpad=10, fontsize=15)
+    plt.ylabel('Sleep_count', color='white', loc='top', labelpad=10, fontsize=15)
+    # plt.title('졸음 횟수', fontsize=16, color='white')
 
-    return render(request, 'main.html', {'y_predict': y_pred_proba, 'temp': temp, 'co2': co2, 'humid':  humid})
+    plt.savefig('/home/crysis/Workspace/5th_ML_Project/Eyes detection-Django/team5_project_django/static/img/graph.png', transparent=True, bbox_inches='tight')
+
+    return render(request, 'main.html', {'y_predict': y_pred_proba, 'temp': temp, 'co2': co2, 'humid': humid})
